9b5c6d2e3b05230d4c467537bc0914ef9f85fc90
Pieter <pieter.schumacher1@gmail.com>
2020-11-04 16:38:50 +0100

    added anchor

---
+++ anchor/anchor.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);
+  if (!rf95.init())
+    Serial.println("init failed");
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      Serial.print("got request: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    }
+  }
+}

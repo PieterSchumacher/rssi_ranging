6d4fd764555aa001490cc5a034b2420135a3a265
Pieter <pieter.schumacher1@gmail.com>
2020-11-04 16:43:00 +0100

    added tag

---
+++ tag/tag.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init())
+    Serial.println("init failed");
+}
+
+void loop()
+{
+  Serial.println("Sending to rf95_server");
+  // Send a message to rf95_server
+  uint8_t data[] = "Hello World!";
+  rf95.send(data, sizeof(data));
+  rf95.waitPacketSent();
+  delay(1000);
+}

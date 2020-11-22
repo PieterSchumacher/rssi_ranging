a6dc59eba862426eda008118b07c1a0a794728b0
Pieter Schumacher <pieter.schumacher1@gmail.com>
2020-11-22 15:09:10 +0100

    /

--- anchor/anchor.ino
+++ anchor/anchor.ino
@@ -1,36 +1,39 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <RHReliableDatagram.h>
 
 #define SF 7
 #define Freq 868.1
 
 // Singleton instance of the radio driver
 RH_RF95 rf95;
+RHReliableDatagram manager(rf95, 2);
 
 void setup() 
 {
   Serial.begin(9600);
   while (!Serial);
-  if (!rf95.init())
+  if (!manager.init())
     Serial.println("init failed");
 }
 
 void loop()
 {
-  if (rf95.available())
+  if (manager.available())
   {
     // Should be a message for us now   
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-    if (rf95.recv(buf, &len))
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
     {
-      Serial.print("got request: ");
+      Serial.print("dist:");
       Serial.println((char*)buf);
-      Serial.print("RSSI: ");
+      Serial.print("RSSI:");
       Serial.println(rf95.lastRssi(), DEC);
-      Serial.print("SNR: ");
+      Serial.print("SNR:");
       Serial.println(rf95.lastSNR(), DEC);
-      Serial.print("SF: ");
+      Serial.print("SF:");
       Serial.println(SF, DEC);
     }
   }
--- rpi_scripts/serial_read.py
+++ rpi_scripts/serial_read.py
@@ -1,8 +1,30 @@
 import serial
-print("Provide the path of serial port: ")
-path = input()
-ser = serial.Serial(path, 9600)
+import re
+import sqlite3
+
+DB_PATH = '/home/pi/rssi_ranging/rssi.db'
+
+conn = sqlite3.connect(DB_PATH)
+c = conn.cursor()
+dist = "dist:"
+rssi = "RSSI:"
+snr = "SNR:"
+d,r,s=-1,-1,-1
+#print("Provide the path of serial port: ")
+#path = input()
+ser = serial.Serial("/dev/ttyACM0", 9600)
 while 1:
-    if(ser.in_waiting >0):
-        line = ser.readline()
-        print(line)
+    if(ser.in_waiting > 0):
+        line = ser.readline().decode("utf-8")
+        line = line[:len(line)-2]
+        m = re.match(dist, line)
+        if m is not None: d = 4.0
+        m = re.match(rssi, line)
+        if m is not None: r = -int(line[m.end()::])
+        m = re.match(snr, line)
+        if m is not None: s = -int(line[m.end()::])
+        if d != -1 and r != -1 and s != -1:
+            print("done ", (d,r,s,))
+            c.execute('INSERT INTO exp1 (Dist,RSSI,SNR) VALUES (?,?,?)', (d,r,s))
+            conn.commit()
+            d,r,s=-1,-1,-1

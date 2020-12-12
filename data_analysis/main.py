73c01fa957a04e4b7e79eccab5f94fcec84afc90
Pieter <pieter.schumacher1@gmail.com>
2020-12-12 14:29:09 +0100

    /

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -10,7 +10,9 @@ conn = sqlite3.connect('../rssi.db')
 conn.row_factory = lambda cursor, row: row[0]
 c = conn.cursor()
 
-dist = c.execute('SELECT DISTINCT dist FROM exp1').fetchall()
+dist = c.execute('SELECT DISTINCT dist FROM exp2').fetchall()
+print(type(dist.remove(0.0)))
+print(dist)
 d = numpy.zeros(len(dist)*NB_SAMPLES)
 rssi = numpy.zeros(len(dist)*NB_SAMPLES)
 
@@ -18,30 +20,70 @@ rssi = numpy.zeros(len(dist)*NB_SAMPLES)
 #     print(i)
 #     print((i)*NB_SAMPLES, ':', (i+1)*NB_SAMPLES)
 #     print(rssi[(i)*NB_SAMPLES:(i+1)*NB_SAMPLES])
-for i in range(len(dist)):
-    d[i*NB_SAMPLES:(i+1)*NB_SAMPLES] = dist[i]
-    rssi[i*NB_SAMPLES:(i+1)*NB_SAMPLES] = numpy.array(random.sample(c.execute('SELECT RSSI FROM exp1 WHERE dist= ?', (dist[i],)).fetchall(), NB_SAMPLES))
-print(d)
-print(rssi)
-
-plt.scatter(d, rssi)
-plt.show()
-
-# MIN-MAX
-d = numpy.zeros(len(dist)*2)
-rssi = numpy.zeros(len(dist)*2)
-for i in range(len(dist)):
-    d[i*2:(i+1)*2] = dist[i]
-    arr = numpy.array(c.execute('SELECT RSSI FROM exp1 WHERE dist= ?', (dist[i],)).fetchall())
-    rssi[i*2:(i+1)*2] = numpy.array([numpy.amin(arr), numpy.amax(arr)])
-plt.scatter(d, rssi)
-plt.show()
+# for i in range(len(dist)):
+#     d[i*NB_SAMPLES:(i+1)*NB_SAMPLES] = dist[i]
+#     rssi[i*NB_SAMPLES:(i+1)*NB_SAMPLES] = numpy.array(random.sample(c.execute('SELECT RSSI FROM exp2 WHERE dist= ?', (dist[i],)).fetchall(), NB_SAMPLES))
+# print(d)
+# print(rssi)
+#
+# plt.scatter(d, rssi)
+# plt.title("Raw RSSI values")
+# plt.ylabel("RSSI (dBm)")
+# plt.xlabel("Distance (m)")
+# plt.show()
+#
+# # MIN-MAX
+# d = numpy.zeros(len(dist)*2)
+# rssi = numpy.zeros(len(dist)*2)
+# for i in range(len(dist)):
+#     d[i*2:(i+1)*2] = dist[i]
+#     arr = numpy.array(c.execute('SELECT RSSI FROM exp2 WHERE dist= ?', (dist[i],)).fetchall())
+#     rssi[i*2:(i+1)*2] = numpy.array([numpy.amin(arr), numpy.amax(arr)])
+# plt.scatter(d, rssi)
+# plt.title("Min-max RSSI values")
+# plt.ylabel("RSSI (dBm)")
+# plt.xlabel("Distance (m)")
+# plt.show()
 
 # AVERAGE
 d = numpy.zeros(len(dist))
 rssi = numpy.zeros(len(dist))
 for i in range(len(dist)):
     d[i] = dist[i]
-    rssi[i] = numpy.average(numpy.array(c.execute('SELECT RSSI FROM exp1 WHERE dist= ?', (dist[i],)).fetchall()))
+    rssi[i] = numpy.average(numpy.array(c.execute('SELECT RSSI FROM exp2 WHERE dist= ?', (dist[i],)).fetchall()))
 plt.scatter(d, rssi)
+plt.title("Average RSSI values")
+plt.ylabel("RSSI (dBm)")
+plt.xlabel("Distance (m)")
+popt = numpy.polyfit(d, rssi, 2)
+xn = numpy.linspace(0, 50, 200)
+yn = numpy.polyval(popt, xn)
+plt.plot(xn, yn)
+# plt.semilogx()
 plt.show()
+
+# # SNR AVERAGE
+# d = numpy.zeros(len(dist))
+# rssi = numpy.zeros(len(dist))
+# for i in range(len(dist)):
+#     d[i] = dist[i]
+#     rssi[i] = numpy.average(numpy.array(c.execute('SELECT SNR FROM exp2 WHERE dist= ?', (dist[i],)).fetchall()))
+# plt.scatter(d, rssi)
+# plt.title("Average SNR values")
+# plt.ylabel("SNR (dBm)")
+# plt.xlabel("Distance (m)")
+# plt.show()
+#
+# # SNR MINMAX
+#
+# d = numpy.zeros(len(dist)*2)
+# rssi = numpy.zeros(len(dist)*2)
+# for i in range(len(dist)):
+#     d[i*2:(i+1)*2] = dist[i]
+#     arr = numpy.array(c.execute('SELECT SNR FROM exp2 WHERE dist= ?', (dist[i],)).fetchall())
+#     rssi[i*2:(i+1)*2] = numpy.array([numpy.amin(arr), numpy.amax(arr)])
+# plt.scatter(d, rssi)
+# plt.title("Raw RSSI values")
+# plt.ylabel("SNR")
+# plt.xlabel("Distance (m)")
+# plt.show()
--- tag/tag.ino
+++ tag/tag.ino
@@ -1,33 +1,59 @@
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <RHReliableDatagram.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 #define SF 7
 #define Freq 868.1
 
 RH_RF95 rf95;
+TinyGPS gps;
+SoftwareSerial ss(4,3);
+
+static void smartdelay(unsigned long ms);
+
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 uint8_t len = sizeof(buf);
+bool newData;
+String datastring="";
+String datastring1="";
+char gps_lon[20]={"\0"};  
+char gps_lat[20]={"\0"}; 
 
 void setup() 
 {
   Serial.begin(9600);
+  ss.begin(9600);
   while (!Serial);
-  if (!rf95.init())
-    Serial.println("init failed");
-    rf95.setFrequency(868.1);
+  if (!rf95.init()) Serial.println("init failed");
+  rf95.setFrequency(868.1);
+      ss.print("Simple TinyGPS library v. "); ss.println(TinyGPS::library_version());
+
 }
 
 void loop()
 {
-  while (Serial.available() == 0) {}
   uint8_t data[sizeof(float)];
-  String b = Serial.readString();
-  b.toCharArray(data, sizeof(data));
+  for (unsigned long start = millis(); millis() - start < 1000;) while (Serial.available()) newData = gps.encode(Serial.read());
+  if (newData) {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    // Once the GPS fixed,send the data to the server.
+    datastring +=dtostrf(flat, 0, 6, gps_lat); 
+    datastring1 +=dtostrf(flon, 0, 6, gps_lon);
+    ss.println(strcat(strcat(gps_lon,","),gps_lat));
+    strcpy(gps_lat,gps_lon);
+    ss.println(gps_lat); //Print gps_lon and gps_lat
+    strcpy((char *)data,gps_lat); 
+  }
+//  while (Serial.available() == 0) {}
+//  String b = Serial.readString();
+//  b.toCharArray(data, sizeof(data));
   for (int i = 0; i < 50; i++) {
     Serial.println(i);
     Serial.print("Sending ");
-    Serial.print(b);
+//    Serial.print(b);
     rf95.send(data, sizeof(data));
     rf95.waitPacketSent();
     if (rf95.waitAvailableTimeout(3000)) { 
@@ -35,11 +61,18 @@ void loop()
         Serial.print("got reply: ");
         Serial.println((char*)buf);
       }
-      else
-        Serial.println("recv failed");
+      else Serial.println("recv failed");
     }
-  else
-    Serial.println("No reply, is rf95_server running?");
-   delay(1000);
+    else Serial.println("No reply, is rf95_server running?");
+    smartdelay(1000);
   }
+}
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
 }

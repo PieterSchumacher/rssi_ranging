9270ca257dadd68dc78c7c18c95af0300c932189
Pieter <pieter.schumacher1@gmail.com>
2021-02-10 00:51:42 +0100

    M

--- tag/tag.ino
+++ tag/tag.ino
@@ -27,23 +27,21 @@ void setup()
 void loop()
 {
   float flat = TinyGPS::GPS_INVALID_F_ANGLE; float flon = TinyGPS::GPS_INVALID_F_ANGLE; unsigned long age = 0;
-  int nb_tries = 10;
+  int nb_tries = 0;
   while(Serial.available() == 0){}
-  while(flat == TinyGPS::GPS_INVALID_F_ANGLE) {smartdelay(3000); gps.f_get_position(&flat, &flon, &age); if (nb_tries>10) break;}
-  if (nb_tries > 10) Serial.println("GPS READING FAILED"); else {
+  String loc = Serial.readString();
+  //while(flat == TinyGPS::GPS_INVALID_F_ANGLE) {smartdelay(3000); gps.f_get_position(&flat, &flon, &age); nb_tries += 1; Serial.println("try"); if (nb_tries>10) break;}
+  //if (nb_tries > 10) Serial.println("GPS READING FAILED"); else {
     Serial.println("GPS READING SUCCESS");
-    for (int i = 0; i < 10; i++) {
-      uint8_t data[sizeof(float)];
-      String b = String(flat); b+=";";
-      b += String(flon); b+=";";
-      b += String(SF); b+=";";
-      b += String(Freq); b+=";";
-      Serial.println(b);
-      b.toCharArray(data, sizeof(data));
-      Serial.println(i);
-      Serial.print("Sending ");
-      Serial.println(b);
-      rf95.send(data, sizeof(data));
+    for (float freq = 860.0; freq < 870; freq += 0.1) {
+      Serial.print("freq: ");
+      Serial.println(freq);
+      uint8_t data_freq[sizeof(float)];
+      String b_freq = String(freq);
+      b_freq += String("%");
+      Serial.println(b_freq);
+      b_freq.toCharArray(data_freq, sizeof(data_freq));
+      rf95.send(data_freq, sizeof(data_freq));
       rf95.waitPacketSent();
       if (rf95.waitAvailableTimeout(3000)) { 
         if (rf95.recv(buf, &len)) {
@@ -52,9 +50,34 @@ void loop()
         }
         else Serial.println("recv failed");
       } else Serial.println("No reply, is rf95_server running?");
-    smartdelay(1000);
+      rf95.setFrequency(freq);
+      Serial.print("freq changed to: ");
+      Serial.println(freq);
+      smartdelay(500);
+      for (int i = 0; i < 10; i++) {
+        uint8_t data[sizeof(float)];
+        String b = String(flat); b+=";";
+        b += String(flon); b+=";";
+        b += loc; b+=";";
+        b += String(freq); b+=";";
+        Serial.println(b);
+        b.toCharArray(data, sizeof(data));
+        Serial.println(i);
+        Serial.print("Sending ");
+        Serial.println(b);
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        if (rf95.waitAvailableTimeout(3000)) { 
+          if (rf95.recv(buf, &len)) {
+            Serial.print("got reply: ");
+            Serial.println((char*)buf);
+          }
+          else Serial.println("recv failed");
+        } else Serial.println("No reply, is rf95_server running?");
+      smartdelay(100);
+      }
     }
-  }
+  //}
 }
 
 static void smartdelay(unsigned long ms)

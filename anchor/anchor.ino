e39e8caaa4fceb7910085e95342732575c0c258a
Pieter <pieter.schumacher1@gmail.com>
2021-03-07 17:20:34 +0100

    /

--- anchor/anchor.ino
+++ anchor/anchor.ino
@@ -1,91 +1,104 @@
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <RHReliableDatagram.h>\
-#include <stdlib.h>
-#include <string.h>
+#include <TinyGPS.h>
+#include <SoftwareSerial.h>
 
 // Singleton instance of the radio driver
 RH_RF95 rf95;
-uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 unsigned long current_millis = millis();
-//char* token1 = "%";
-//char* token2 = ";";
 float lastRssi;
 float lastSNR;
-float freq;
-uint8_t p;
-uint8_t cr;
-uint8_t sf;
-float flat;
-float flong;
 
-int change_config(char* buf) {
-  return !strcmp(buf + (strlen(buf)-strlen("%")), "%");
-}
+TinyGPS gps;
+SoftwareSerial ss(4, 3);
+
+static void smartdelay(unsigned long ms);
+
+struct Config {
+  uint8_t sf;
+  uint8_t cr;
+  uint8_t p;
+  float freq;
+  float flat;
+  float flong;
+};
+
+union Data {
+  uint8_t buf[sizeof(struct Config)];
+  Config con;
+} data;
+
 
 void setup() 
 {
   Serial.begin(9600);
   while (!Serial);
   if (!rf95.init())
-    Serial.println("init failed");
-}
-
-void loop()
-{
-//  if (true) {
-//    current_millis = millis();
-//    Serial.print("RSSI:");
-//    Serial.println(1.1111111, DEC);
-//    Serial.print("SNR:");
-//    Serial.println(1.1111111, DEC);
-//    Serial.println(millis()-current_millis, DEC);
-//  } else {
-  rf95.init();
+  Serial.println("init failed");
   rf95.setFrequency(863.1);
   rf95.setSpreadingFactor(7);
   rf95.setCodingRate4(5);
-  rf95.setTxPower(18);
-  current_millis = millis();
-  Serial.println("default config");
-  while(!rf95.available()) {}
-  while(millis() - current_millis < 5000) {
-    if (rf95.available()) {
-      uint8_t len = sizeof(buf);
-      if (rf95.recv(buf, &len)) {
+  rf95.setTxPower(18);   
+}
+
+void loop()
+{if (rf95.available()) {
+  uint8_t len = sizeof(data.buf);
+
+ if (rf95.recv(data.buf, &len)) {
+    Serial.println("CONFIG PACKET: ");
+    Serial.println(data.con.sf, DEC);
+    Serial.println(data.con.cr, DEC);
+    Serial.println(data.con.p, DEC);
+    Serial.println(data.con.freq, 1);
+    Serial.println(data.con.flat, 6);
+    Serial.println(data.con.flong, 6);
+//    rf95.init();
+    rf95.setSpreadingFactor(data.con.sf);
+    rf95.setCodingRate4(data.con.cr);
+    rf95.setFrequency(data.con.freq);
+    rf95.setTxPower(data.con.p);
+    smartdelay(3000);
+
+    current_millis = millis();
+    
+    uint8_t message[1]; 
+    len = sizeof(message);
+    while(millis() - current_millis < 10000) {
+      if (rf95.recv(message, &len)) {
         current_millis = millis();
-        if (change_config(buf)) {
-          Serial.println("hi!");
-          sscanf(buf, "%u;%u;%f;%u;%f,%f%", &sf, &cr, &freq, &p, &flat, &flong);
-          rf95.init();
-          rf95.setSpreadingFactor(sf);
-          rf95.setCodingRate4(cr);
-          rf95.setFrequency(freq);
-          rf95.setTxPower(p);
-          Serial.println(sf, DEC);
-          Serial.println(cr, DEC);
-          Serial.println(freq, DEC);
-          Serial.println(p, DEC);
-          Serial.println(flat, DEC);
-          Serial.println(flong, DEC);
-        } else {
-          lastRssi = rf95.lastRssi();
-          lastSNR = rf95.lastSNR();
-          Serial.print("RSSI:");
-          Serial.println(rf95.lastRssi(), DEC);
-          Serial.print("SNR:");
-          Serial.println(rf95.lastSNR(), DEC);
-          Serial.print("FREQ:");
-          Serial.println(freq, DEC);
-          Serial.print("TxPower:");
-          Serial.println(p, DEC);
-          Serial.print("CR:");
-          Serial.println(cr, DEC);
-          Serial.print("SF:");
-          Serial.println(sf, DEC);
-        }
+        lastRssi = rf95.lastRssi();
+        lastSNR = rf95.lastSNR();
+        Serial.println(message[0]);
       }
     }
+//        Serial.print("RSSI:");
+//        Serial.println(rf95.lastRssi(), 3);
+//        Serial.print("SNR:");
+//        Serial.println(rf95.lastSNR(), 3);
+//        Serial.print("FREQ:");
+//        Serial.println(freq, 3);
+//        Serial.print("TxPower:");
+//        Serial.println(p, DEC);
+//        Serial.print("CR:");
+//        Serial.println(cr, DEC);
+//        Serial.print("SF:");
+//        Serial.println(sf, DEC);   
+//    rf95.init();
+    rf95.setFrequency(863.1);
+    rf95.setSpreadingFactor(7);
+    rf95.setCodingRate4(5);
+    rf95.setTxPower(18);   
   }
-//  }
+}
+}
+
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
 }
--- tag/tag.ino
+++ tag/tag.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <RHReliableDatagram.h>
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
 
@@ -10,14 +9,29 @@
 RH_RF95 rf95;
 TinyGPS gps;
 SoftwareSerial ss(4, 3);
-uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-uint8_t len = sizeof(buf);
 
 float latitude = 5.31;
 float longitude = 5.31;
 unsigned long age = 0;
+const uint8_t message[1] = {1};
+uint8_t buf[30];
+
 
 static void smartdelay(unsigned long ms);
+
+struct Config {
+  uint8_t sf;
+  uint8_t cr;
+  uint8_t p;
+  float freq;
+  float flat;
+  float flong;
+};
+
+union Data {
+  uint8_t buf[sizeof(struct Config)];
+  Config con;
+} data;
 
 void setup() 
 {
@@ -25,6 +39,9 @@ void setup()
   while (!Serial);
   if (!rf95.init()) Serial.println("init failed");
   rf95.setFrequency(default_freq);
+  rf95.setSpreadingFactor(7);
+  rf95.setCodingRate4(5);
+  rf95.setTxPower(18);
   ss.begin(9600);
 }
 
@@ -32,35 +49,29 @@ void loop()
 {
   for (uint8_t sf = 7; sf < 13; sf += 1) {
     for (uint8_t cr = 5; cr < 9; cr += 1) {
-      for (float freq = 863.1; freq < 870; freq += 0.2) {
-        for (uint8_t p = 2; p < 21; p += 1) {
+      for (uint8_t p = 2; p < 21; p += 1) {
+        for (float freq = 863.1; freq < 870; freq += 0.2) {
           Serial.println("new config");
-          rf95.init(); 
+//          rf95.init(); 
           rf95.setFrequency(default_freq);
           rf95.setSpreadingFactor(7);
           rf95.setCodingRate4(5);
           rf95.setTxPower(18);
-          String b_freq = String(sf); b_freq += ";";
-          b_freq += String(cr); b_freq += ";";
-          b_freq += String(freq); b_freq += ";";
-          b_freq += String(p); b_freq += ";";
-          while(latitude == TinyGPS::GPS_INVALID_F_ANGLE) {smartdelay(1000); gps.f_get_position(&latitude, &longitude, &age);}
-          
-          b_freq += String(latitude,6); b_freq += String(","); b_freq += String(longitude,6);
-          b_freq += String("%"); 
-          b_freq.toCharArray(buf, sizeof(buf));
-          Serial.println(b_freq);
-          rf95.send(buf, sizeof(buf));
+          smartdelay(4000);
+          while(latitude == TinyGPS::GPS_INVALID_F_ANGLE) {latitude = 5.31; longitude = 5.31;}//gps.f_get_position(&latitude, &longitude, &age);}
+
+          data.con = {sf, cr, p, freq, latitude, longitude};
+          rf95.send(data.buf, sizeof(data.buf));
           rf95.waitPacketSent();
-          rf95.init();
+//          rf95.init();
           rf95.setSpreadingFactor(sf);
           rf95.setCodingRate4(cr);
           rf95.setFrequency(freq);
           rf95.setTxPower(p);
-          smartdelay(2000);
+          smartdelay(5000);
+          Serial.println("send new data packet");
           for (int i = 0; i < 10; i++) {
-            Serial.println("send new data packet");
-            rf95.send(buf, sizeof(buf));
+            rf95.send(message, sizeof(message));
             rf95.waitPacketSent();
           }
           latitude = TinyGPS::GPS_INVALID_F_ANGLE; longitude = TinyGPS::GPS_INVALID_F_ANGLE; 
@@ -70,6 +81,7 @@ void loop()
     }
   }
 }
+
 
 static void smartdelay(unsigned long ms)
 {

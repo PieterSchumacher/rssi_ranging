c67adb2a1bb01c02749dc754ace1ed8a92cbd44e
Pieter Schumacher <pieter.schumacher1@gmail.com>
2021-03-16 18:58:55 +0100

    /

--- rpi_scripts/serial_read.py
+++ rpi_scripts/serial_read.py
@@ -11,21 +11,21 @@ ser.setDTR(1)
 size = struct.calcsize('fffB');
 float_size = struct.calcsize('ff');
 while 1:
-    try:
+    #try:
         if ser.in_waiting > 0:
             a_loc = struct.unpack('ff', ser.read(float_size))
             config = struct.unpack('fffB', ser.read(size))
             time.sleep(.1)
             current_millis = time.time()
             while time.time() - current_millis < 0.6:
-                try:
+                #try:
                     if ser.in_waiting > 0:
                         data = struct.unpack('hh', ser.read(4))
                         current_millis = time.time()
                         c.execute('INSERT INTO exp4 (RSSI,SNR,T_FLAT,T_FLONG,A_FLAT,A_FLONG,FREQ,SF) VALUES (?,?,?,?,?,?,?,?)',
-                                                    (data[0], data[1], config[1], config[2], a_loc[0], a_loc[1], config[0], config[3]))
-                except:
-                    print("error in data packets")
+                                                    (data[0], data[1], round(config[1],6), round(config[2],6), round(a_loc[0],6), round(a_loc[1],6), round(config[0],1), config[3]))
+                #except:
+                    #print("error in data packets")
             conn.commit()
-    except:
-        print("error in config packets")
+    #except:
+        #print("error in config packets")
